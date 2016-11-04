6005705f420094fe82ae00b37fa4761d27589a61
WenjingWangCarrie <wenjing.liu.wang@gmail.com>
2016-11-04 01:00:53 -0400

    Add files via upload

--- Lab5/index.php
+++ Lab5/index.php
@@ -1,1 +1,27 @@
+<?php
+$firstName = "Wenjing";
+$lastName = "Wang";
+define("STUDENT_NUM", "040812907");
+$sentence = "Hello World! ";
+$sentences = "I am using PHP!";
 
+echo "First Name: ".$firstName;
+echo "<br />";
+echo "Last Name: ".$lastName;
+echo "<br />";
+echo "Student Number: ".STUDENT_NUM;
+echo "<br />";
+echo $sentence.$sentences;
+echo "<br />";
+
+echo '<a href="http://wenjingwang.com/CST8238/Lab1/index.html">Lab1</a>';
+echo "<br />";
+echo '<a href="http://wenjingwang.com/CST8238/Lab2/index.html">Lab2</a>';
+echo "<br />";
+echo '<a href="http://wenjingwang.com/CST8238/Lab3/index.html">Lab3</a>';
+echo "<br />";
+echo '<a href="http://wenjingwang.com/CST8238/Lab4/index.html">Lab4</a>';
+echo "<br />";
+echo '<a href="http://wenjingwang.com/CST8238/Assignment1/Index.html">Assignment1</a>';
+
+?>
